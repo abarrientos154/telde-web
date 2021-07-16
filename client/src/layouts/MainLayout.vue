01e98428ec401e6d15aa548b7f016cc8f3d87d6d
Magdy <example@example.com>
2021-07-16 11:52:56 -0400

    fix

--- client/src/layouts/MainLayout.vue
+++ client/src/layouts/MainLayout.vue
@@ -3,46 +3,37 @@
     <q-header elevated>
         <q-toolbar class="row justify-between">
           <div>
-            <q-img src="logo_nova_telde.png" style="width:190px; height:70px" />
+            <q-img src="logo_nova_telde.png" style="width:220px; height:80px" />
           </div>
 
-          <div v-if="rol !== 1" class="row justify-around items-center q-gutter-sm" >
-          <div><q-btn :label="rol === 3 ? 'Tienda' : 'Home'" color="white" flat stack dense no-caps size="md" @click="rol === 3 ? $router.push('/tienda/'+user_id) : $router.push('/inicio')" /></div>
-          <div><q-btn :label="rol === 3 ? 'Monedero' : 'Tiendas'" color="white" flat stack dense no-caps size="md" @click="rol === 3 ? $router.push('/monedero/') : $router.push('/tiendas')" /></div>
-          <div><q-btn :label="rol === 3 ? 'Estadísticas' : 'Favoritos'" color="white" flat stack dense no-caps size="md" @click="rol === 2 ? $router.push('/tiendas_favoritas') : $router.push('/estadisticas')" /></div>
-          <div><q-btn label="Pedidos" color="white" flat stack dense no-caps size="md" @click="rol === 3 ? $router.push('/mis_pedidos_tienda') : $router.push('/mis_pedidos')" /></div>
-          <div><q-btn label="Salir" color="white" flat stack dense no-caps size="md" @click="cerrarSesion()" /></div>
-        </div>
-        <div v-else class="row justify-around items-center q-gutter-sm" >
-          <div><q-btn icon="home" label="Home" color="white" flat stack dense no-caps size="md" @click="$router.push('/administrador')" /></div>
-          <div><q-btn icon="groups" label="Usuarios" color="white" flat stack dense no-caps size="md" @click="$router.push('/usuarios')" /></div>
-          <div><q-btn icon="monetization_on" label="Retiros" color="white" flat stack dense no-caps size="md" @click="$router.push('/retiros')" /></div>
-          <div><q-btn icon="description" label="Estadísticas" color="white" flat stack dense no-caps size="md" @click="$router.push('/reportes')" /></div>
-          <div><q-btn icon="logout" label="Salir" color="white" flat stack dense no-caps size="md" @click="cerrarSesion()" /></div>
-        </div>
+          <div v-if="rol !== 0">
+            <div v-if="rol !== 1" class="row justify-around items-center q-gutter-md" >
+              <div><q-btn :label="rol === 3 ? 'Tienda' : 'Inicio'" color="white" flat dense no-caps size="lg" @click="rol === 3 ? $router.push('/tienda/'+user_id) : $router.push('/inicio')" /></div>
+              <div><q-btn :label="rol === 3 ? 'Monedero' : 'Tiendas'" color="white" flat dense no-caps size="lg" @click="rol === 3 ? $router.push('/monedero/') : $router.push('/tiendas')" /></div>
+              <div><q-btn :label="rol === 3 ? 'Estadísticas' : 'Favoritos'" color="white" flat dense no-caps size="lg" @click="rol === 2 ? $router.push('/tiendas_favoritas') : $router.push('/estadisticas')" /></div>
+              <div><q-btn label="Pedidos" color="white" flat dense no-caps size="lg" @click="rol === 3 ? $router.push('/mis_pedidos_tienda') : $router.push('/mis_pedidos')" /></div>
+              <div><q-btn label="Cerrar sesión" color="white" flat dense no-caps size="lg" @click="cerrarSesion()" /></div>
+            </div>
+            <div v-else class="row justify-around items-center q-gutter-md" >
+              <div><q-btn label="Home" color="white" flat dense no-caps size="lg" @click="$router.push('/administrador')" /></div>
+              <div><q-btn label="Usuarios" color="white" flat dense no-caps size="lg" @click="$router.push('/usuarios')" /></div>
+              <div><q-btn label="Retiros" color="white" flat dense no-caps size="lg" @click="$router.push('/retiros')" /></div>
+              <div><q-btn label="Estadísticas" color="white" flat dense no-caps size="lg" @click="$router.push('/reportes')" /></div>
+              <div><q-btn label="Cerrar sesión" color="white" flat dense no-caps size="lg" @click="cerrarSesion()" /></div>
+            </div>
+          </div>
+
+          <div v-else class="row justify-around items-center q-gutter-md">
+            <div><q-btn label="Inicio" color="white" flat dense no-caps size="lg" @click="$router.push('/inicio')" /></div>
+            <div><q-btn label="Nosotros" color="white" flat dense no-caps size="lg"/></div>
+            <div><q-btn label="Iniciar sesión" color="white" flat dense no-caps size="lg" @click="$router.push('/login')" /></div>
+          </div>
         </q-toolbar>
       </q-header>
 
     <q-page-container class="q-mb-md">
       <router-view />
     </q-page-container>
-
-      <q-footer elevated v-if="rol != 0">
-        <div v-if="rol !== 1" class="bg-blue-14 shadow-2 full-width row justify-around" >
-          <div class="row items-center"><q-btn :icon="rol === 3 ? 'store' : 'home'" :label="rol === 3 ? 'Tienda' : 'Home'" color="white" flat stack dense no-caps size="md" @click="rol === 3 ? $router.push('/tienda/'+user_id) : $router.push('/inicio')" /></div>
-          <div class="row items-center"><q-btn :icon="rol === 3 ? 'monetization_on' : 'store'" :label="rol === 3 ? 'Monedero' : 'Tiendas'" color="white" flat stack dense no-caps size="md" @click="rol === 3 ? $router.push('/monedero/') : $router.push('/tiendas')" /></div>
-          <div class="row items-center"><q-btn :icon="rol === 3 ? 'description' : 'favorite'" :label="rol === 3 ? 'Estadísticas' : 'Favoritos'" color="white" flat stack dense no-caps size="md" @click="rol === 2 ? $router.push('/tiendas_favoritas') : $router.push('/estadisticas')" /></div>
-          <div class="row items-center"><q-btn icon="shopping_basket" label="Pedidos" color="white" flat stack dense no-caps size="md" @click="rol === 3 ? $router.push('/mis_pedidos_tienda') : $router.push('/mis_pedidos')" /></div>
-          <div class="row items-center"><q-btn icon="logout" label="Salir" color="white" flat stack dense no-caps size="md" @click="cerrarSesion()" /></div>
-        </div>
-        <div v-else class="bg-blue-14 shadow-2 full-width row justify-around" >
-          <div class="row items-center"><q-btn icon="home" label="Home" color="white" flat stack dense no-caps size="md" @click="$router.push('/administrador')" /></div>
-          <div class="row items-center"><q-btn icon="groups" label="Usuarios" color="white" flat stack dense no-caps size="md" @click="$router.push('/usuarios')" /></div>
-          <div class="row items-center"><q-btn icon="monetization_on" label="Retiros" color="white" flat stack dense no-caps size="md" @click="$router.push('/retiros')" /></div>
-          <div class="row items-center"><q-btn icon="description" label="Estadísticas" color="white" flat stack dense no-caps size="md" @click="$router.push('/reportes')" /></div>
-          <div class="row items-center"><q-btn icon="logout" label="Salir" color="white" flat stack dense no-caps size="md" @click="cerrarSesion()" /></div>
-        </div>
-      </q-footer>
   </q-layout>
 </template>
 
